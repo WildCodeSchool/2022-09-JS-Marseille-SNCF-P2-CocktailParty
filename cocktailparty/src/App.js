dafb54160b7783caa0c7c6557b8a153fa6ebe8dd
geoffont <geoffroy.font@gmail.com>
2022-11-25 14:40:45 +0100

    installation axios et MUI

--- cocktailparty/src/App.js
+++ cocktailparty/src/App.js
@@ -1,11 +1,11 @@
-
+import Search from './components/search/search';
 import './App.css';
 
 function App() {
   return (
     <div>
       <div>
-        
+      <Search />
       </div>
       
     </div>
