0edb6389119a5bf7ababfb4675120b2a3c46a012
geoffont <geoffroy.font@gmail.com>
2022-11-30 16:44:16 +0100

    mise en place des cartes cocktails

--- cocktailparty/src/App.js
+++ cocktailparty/src/App.js
@@ -1,50 +1,47 @@
-import SearchBar from './components/search/Search';
-import './App.css';
-import Cocktail from './components/Cocktail/Cocktail';
-import axios from 'axios';
-import React, { useState } from 'react';
-import Footer from './components/footer/Footer';
-
+import SearchBar from "./components/search/Search";
+import "./App.css";
+import Cocktail from "./components/Cocktail/Cocktail";
+import axios from "axios";
+import React, { useState } from "react";
+import Footer from "./components/footer/Footer";
+import CocktailList from "./components/CocktailList/CocktailList";
 
 const firstDrink = {
-  strDrink: 'name',
-  strImageSource: "https://commons.wikimedia.org/wiki/File:Klassiche_Margarita.jpg",
+  strDrink: "name",
+  strImageSource:
+    "https://commons.wikimedia.org/wiki/File:Klassiche_Margarita.jpg",
   strIngredient1: "Tequila",
- };
+};
 
 function App() {
   const [drink, setDrink] = useState(firstDrink);
 
   const getDrink = () => {
-    
     axios
-      .get('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
-      
+      .get("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
+
       .then((response) => response.data)
-     
+
       .then((data) => {
-        
-        
         console.log(data);
       });
-      
   };
   return (
-    <div className='App'>
-      
+    <div className="App">
+      <SearchBar />
 
-      
-     <SearchBar />
-
-     <Cocktail
+      <Cocktail
         strDrink={drink.strDrink}
         strImageSource={drink.strImageSource}
         strIngredient1={drink.strIngredient1}
         getDrink={getDrink}
-      /> 
-
-      <Footer/>
-
+      />
+<div className="cocktail-grid">
+      <CocktailList />
+      <CocktailList/>
+      <CocktailList/>
+      </div>
+      <Footer />
     </div>
   );
 }
