222b564ea0889f9cc842a5a8226c20ca5277d71c
sansan28 <sandra.taboada@sncf.fr>
2022-12-01 09:22:13 +0100

    Added  a loop in the searchbar and modified CSS Style to search component

--- cocktailparty/src/components/search/search.js
+++ cocktailparty/src/components/search/search.js
@@ -2,22 +2,27 @@ import "./style.css";
 
 
 
+
 const SearchBar = () => (
   <div className="searchbar">
+    
     <form className="form">
       <input
         className="enter"
         type="text"
         id="header-search"
         placeholder="Votre cocktail ou ingrédient.."
-        name="searchBar"
+        name="q"
       />
-      <button className="btn" type="submit">
-        Recherche
-      </button>
+      <div className="loop">
+        <i class="fa-solid fa-magnifying-glass"></i>
+      </div>
     </form>
+    <button className="btn" type="submit">
+      Recherche
+    </button>
   </div>
 );
-//
+
 
 export default SearchBar;
