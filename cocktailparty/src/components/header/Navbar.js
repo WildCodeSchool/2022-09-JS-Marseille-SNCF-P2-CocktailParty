5ec3421ff37c36fb18e19f909b1fa9fc8fb9f2e0
BettyDetroit <caubelnelly@gmail.com>
2022-12-19 20:59:52 +0100

    last icon items unused removed from navbar.js

--- cocktailparty/src/components/header/Navbar.js
+++ cocktailparty/src/components/header/Navbar.js
@@ -4,7 +4,6 @@ import * as AiIcons from "react-icons/ai";
 import { Link } from "react-router-dom";
 import { SidebarData } from "./SidebarData";
 import "./navbar.css";
-import { IconContext } from "react-icons";
 
 function Navbar() {
   const [sidebar, setSidebar] = useState(false);
@@ -13,7 +12,6 @@ function Navbar() {
 
   return (
     <>
-      <IconContext.Provider value={{ color: "undefined" }}>
         <div className="navbar">
           <Link to="#" className="menu-bars">
             <FaIcons.FaBars onClick={showSidebar} />
@@ -30,7 +28,6 @@ function Navbar() {
               return (
                 <li key={index} className={item.cName}>
                   <Link to={item.path}>
-                    {item.icon}
                     <span>{item.title}</span>
                   </Link>
                 </li>
@@ -38,7 +35,6 @@ function Navbar() {
             })}
           </ul>
         </nav>
-      </IconContext.Provider>
     </>
   );
 }
